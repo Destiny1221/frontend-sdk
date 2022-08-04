28439eff10c56822a74d988cbc48fa9e386da10d
zhenyu.yu <zhenyu.yu@pinlandata.com>
2022-08-04 18:28:38 +0800

    增加eslint和pretter配置

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@
  * @Author: yuzy
  * @Date: 2022-08-03 11:12:14
  * @LastEditors: yuzy
- * @LastEditTime: 2022-08-03 17:17:18
+ * @LastEditTime: 2022-08-04 17:43:32
  * @Description:
  */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -21,12 +22,19 @@ module.exports = {
       filename: 'index.html', // 自动生成HTML文件的名称
       favicon: path.resolve(__dirname, 'public/logo.png'), // 设置页面icon
     }),
+    //  添加eslint校验
+    new ESLintPlugin({
+      extensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
+      context: path.resolve(__dirname, 'src'),
+      exclude: '/node_modules',
+      fix: true /* 自动帮助修复 */,
+    }),
   ],
   module: {
     rules: [
       {
         test: /.ts$/,
-        use: ['ts-loader'],
+        use: ['babel-loader', 'ts-loader'],
         exclude: /node_modules/,
       },
     ],
